b24f5e48a2995d4dc903be05550c5cbc4b22931f
Ben LaVerriere <benlaverriere@gmail.com>
2021-02-20 14:17:10 -0500

    Simplify to a single pandoc invocation

--- generate_build.rb
+++ generate_build.rb
@@ -8,21 +8,30 @@ require 'active_support/inflector'
 
 BUILD_FILE = 'build.ninja'
 INPUT_FILE_GLOB = './plain/*.md'
-RENDER_PAGE_RULE = <<~RULE
-rule recipe2pdf
-  command = pandoc -f markdown -t html --variable papersize:letter -s -o $out $in
-  description = recipe2pdf $in $out
+FULL_OUTPUT_FILE = './rendered/full.pdf'
+
+COMBINE_PAGES_RULE = <<~RULE
+rule render
+  command = pandoc -f markdown -t html --variable papersize:letter --file-scope -o $out $in
+  description = render $out ← $in
 
 RULE
 
 File.open(BUILD_FILE, "w") { |f|
-  input_files = Dir[INPUT_FILE_GLOB]
-  f.write RENDER_PAGE_RULE
-  input_files.each { |file|
-    basename = File.basename(file, '.*')
-    f.write "build rendered/singles/#{basename}.pdf: recipe2pdf #{file}\n"
+  f.write COMBINE_PAGES_RULE
+
+  input_files = Dir[INPUT_FILE_GLOB].sort
+  output_files = []
+  input_files.each { |input_file|
+    basename = File.basename(input_file, '.*')
+    output_file = "rendered/singles/#{basename}.pdf"
+    output_files << output_file
+
+    # f.write "build #{output_file}: recipe2pdf #{input_file}\n"
   }
-  f.write "\n"
-  rule_count = input_files.size
-  puts "Wrote #{rule_count} #{"rule".pluralize(rule_count)} to #{BUILD_FILE}."
+
+  f.write "build #{FULL_OUTPUT_FILE}: render #{input_files.join(' ')}\n"
+
+  recipe_count = input_files.size
+  puts "Wrote build script for #{recipe_count} #{"recipe".pluralize(recipe_count)} to #{BUILD_FILE}."
 }
