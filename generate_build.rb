ea80792dae6f459cf945ce2986d78a21ab8aa8d6
Ben LaVerriere <benlaverriere@gmail.com>
2021-02-20 14:39:49 -0500

    Let the build script regenerate itself

--- generate_build.rb
+++ generate_build.rb
@@ -11,15 +11,23 @@ INPUT_FILE_GLOB = './plain/*.md'
 FULL_OUTPUT_FILE = './rendered/full.pdf'
 CSS_FILE = 'print.css'
 
+GENERATOR_RULE = <<~RULE
+rule generate
+  command = ruby generate_build.rb
+  description = generate
+  generator = 1
+RULE
+
 COMBINE_PAGES_RULE = <<~RULE
 rule render
   command = pandoc -f markdown -t html --css #{CSS_FILE} --variable papersize:letter --file-scope -o $out $in
   description = render $out ← $in
-
 RULE
 
 File.open(BUILD_FILE, "w") { |f|
-  f.write COMBINE_PAGES_RULE
+  f.puts GENERATOR_RULE
+  f.puts COMBINE_PAGES_RULE
+  f.puts "build #{BUILD_FILE}: generate | generate_build.rb"
 
   input_files = Dir[INPUT_FILE_GLOB].sort
   output_files = []
@@ -27,11 +35,9 @@ File.open(BUILD_FILE, "w") { |f|
     basename = File.basename(input_file, '.*')
     output_file = "rendered/singles/#{basename}.pdf"
     output_files << output_file
-
-    # f.write "build #{output_file}: recipe2pdf #{input_file}\n"
   }
 
-  f.write "build #{FULL_OUTPUT_FILE}: render #{input_files.join(' ')} | #{CSS_FILE}\n"
+  f.puts "build #{FULL_OUTPUT_FILE}: render #{input_files.join(' ')} | #{CSS_FILE}"
 
   recipe_count = input_files.size
   puts "Wrote build script for #{recipe_count} #{"recipe".pluralize(recipe_count)} to #{BUILD_FILE}."
